aaab7bc31276d7cf69855ef1f39ea72a9994d7d9
AtsukiUsui <kmmm13037@gmail.com>
2023-09-28 19:15:24 +0900

    一回service投げると停止するようにした

--- scripts/play_service.py
+++ scripts/play_service.py
@@ -6,7 +6,6 @@ import subprocess
 import time
 import sensor_msgs.msg
 
-
 # コールバック関数：/diag_scan トピックからメッセージを受信したときに呼ばれる
 
 
@@ -29,18 +28,26 @@ if __name__ == '__main__':
     # 最後のメッセージ受信時刻を初期化
     last_msg_time = rospy.get_time()
 
+    # rosservice call を実行したかどうかを示すフラグ
+    called_service = False
+
     # メインループ
     rate = rospy.Rate(1)  # 1Hzのループ
     while not rospy.is_shutdown():
         # 最後のメッセージ受信から10秒以上経過していれば、rosservice call を実行
         current_time = rospy.get_time()
-        if current_time - last_msg_time > 10.0:
+        if current_time - last_msg_time > 10.0 and not called_service:
             rospy.logwarn(
                 "No messages received for 10 seconds. Calling rosservice...")
             try:
                 # rosservice call コマンドを実行
                 subprocess.call(["rosservice", "call", "/up_map"])
+                called_service = True  # サービスを一度だけ呼び出す
             except Exception as e:
                 rospy.logerr("Error calling rosservice: {}".format(e))
+
+        if called_service:
+            rospy.loginfo("rosservice call completed. Exiting...")
+            break
 
         rate.sleep()
